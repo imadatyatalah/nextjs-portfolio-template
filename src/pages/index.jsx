4fe05582b5eb4245f406140991b5ea6d841a12de
imad <imadatyatalah@gmail.com>
2021-04-28 12:01:28 +0000

    feat: Make it more responsive for desktops

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,26 +5,31 @@ import Button from "@/components/buttons";
 const Home = () => {
   return (
     <>
-      <section className="py-6 flex flex-col justify-center items-center text-center">
-        <div className="relative rounded-full my-4 w-80 h-80 sm:w-96 sm:h-96">
-          <Image
-            src="/user-avatar.jpg"
-            layout="fill"
-            alt="avatar"
-            className="avatar"
-          />
+      <section className="px-[30px] md:px-[60px] lg:px-[120px]">
+        <div className="py-6 flex flex-col justify-center items-center text-center lg:flex-row-reverse lg:text-left lg:justify-between">
+          <div className="relative rounded-full my-4 w-80 h-80 sm:w-[22rem] sm:h-[22rem] lg:w-96 lg:h-96">
+            <Image
+              src="/user-avatar.jpg"
+              layout="fill"
+              alt="avatar"
+              className="rounded-full bg-light"
+            />
+          </div>
+
+          <div className="lg:w-1/2">
+            <h1 className="text-[32px] font-bold text-dark px-4 lg:px-0 lg:text-[44px]">
+              Hi, I'm John, Creative Technologist
+            </h1>
+            <p className="text-dark my-4 px-4 lg:px-0">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+              Exercitation veniam consequat sunt nostrud amet.
+            </p>
+            <Button color="primary" size="default">
+              Download Resume
+            </Button>
+          </div>
         </div>
-        <h1 className="text-3xl font-bold text-dark px-4">
-          Hi, I'm John, Creative Technologist
-        </h1>
-        <p className="text-dark px-4 my-4">
-          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-          sint. Velit officia consequat duis enim velit mollit. Exercitation
-          veniam consequat sunt nostrud amet.
-        </p>
-        <Button color="primary" size="default">
-          Download Resume
-        </Button>
       </section>
     </>
   );
